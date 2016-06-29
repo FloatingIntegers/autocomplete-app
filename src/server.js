59bcfea1fcbbe1ca7993fe8e9979d3e47e509e79
Matt Glover <matthewglover@me.com>
2016-06-29 15:59:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,3 @@
-const fs = require('fs');
-
-function handler(req, res) {
-  const url = req.url;
-
-  if (url === '/') {
-    fs.readFile(__dirname + '/..' + '/index.html', (err, data) => {
-      if (err){
-          throw err;
-      }
-      res.writeHead(200, {"Content-type": "text/html"});
-      res.end(data);
-    });
-} else {
-        fs.readFile(__dirname + url, (err, data) => {
-        if (err) {
-          res.writeHead(404);
-          res.end('<h2>404 File not found</h2>');
-        }
-      });
-    }
-}
-
-
-module.exports = handler;
+const http = require('http');
+const handler = require('./src/handler.js');
+const port = process.env.PORT || 4000;
