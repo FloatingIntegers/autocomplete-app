21e8002ab413adc9c8364047f6531297f84f80a0
Sofia <emmapohjalainen@gmail.com>
2016-06-29 15:35:24 +0100

    Adds two tests for backend that pass
    EXTRA MOTIVATION NYANCAT TEST

---
+++ src/handler.js
@@ -0,0 +1,25 @@
+    const fs = require('fs');
+
+    function handler(req, res) {
+      const url = req.url;
+
+      if (url === '/') {
+        fs.readFile(__dirname + '/..' + '/index.html', (err, data) => {
+          if (err){
+              throw err;
+          }
+          res.writeHead(200, {"Content-type": "text/html"});
+          res.end(data);
+        });
+    } else {
+            fs.readFile(__dirname + url, (err, data) => {
+            if (err) {
+              res.writeHead(404);
+              res.end('<h2>404 File not found</h2>');
+            } 
+          });
+        }
+    }
+
+
+    module.exports = handler;
