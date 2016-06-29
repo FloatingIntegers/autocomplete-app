c7a0390816469422246ede5a691e82720ef939bb
Matt Glover <matthewglover@me.com>
2016-06-29 16:04:28 +0100

    Add server.js back in

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 const http = require('http');
 const handler = require('./src/handler.js');
 const port = process.env.PORT || 4000;
+
+http.createServer(handler).listen(port);
+
+console.log("Server is running on localhost:" + port);
