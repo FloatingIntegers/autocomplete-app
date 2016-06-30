5b5bf9a08fa4f1877e5ed159bff7dd9b19a4f3db
Matt Glover <matthewglover@me.com>
2016-06-30 12:35:38 +0100

    add get-query-string.js for querystring handling

--- src/handler.js
+++ src/handler.js
@@ -1,38 +1,38 @@
-    const fs = require('fs');
+const fs = require('fs');
+const getQueryParams = require('./get-query-params');
 
-    function handler(req, res) {
-      const url = req.url;
 
-      if (url === '/') {
-        fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
-          if (err) {
-            throw err;
-          }
-          res.writeHead(200, { 'Content-type': 'text/html' });
-          res.end(data);
-        });
-      } else if (url.includes('api/words')) {
-        res.writeHead(200, { 'Content-type': 'text/plain' });
-        res.end('blue\nbrown\ngreen');
-      } else if (url.includes('public')) {
-        const ext = url.split('.')[1];
-        fs.readFile(`${__dirname}/..${url}`, (err, data) => {
-          if (err) {
-            res.writeHead(404);
-            res.end('<h2>404 File not found</h2>');
-          }
-          res.writeHead(200, { 'Content-type': `text/${ext}` });
-          res.end(data);
-        });
-      } else {
-        fs.readFile(__dirname + url, (err) => {
-          if (err) {
-            res.writeHead(404);
-            res.end('<h2>404 File not found</h2>');
-          }
-        });
+function handler(req, res) {
+  if (req.url === '/') {
+    fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
+      if (err) {
+        throw err;
       }
-    }
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(data);
+    });
+  } else if (req.url.includes('api/words')) {
+    console.log(getQueryParams(req.url));
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('blue\nbrown\ngreen');
+  } else if (req.url.includes('public')) {
+    const ext = req.url.split('.')[1];
+    fs.readFile(`${__dirname}/..${req.url}`, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('<h2>404 File not found</h2>');
+      }
+      res.writeHead(200, { 'Content-type': `text/${ext}` });
+      res.end(data);
+    });
+  } else {
+    fs.readFile(__dirname + req.url, (err) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('<h2>404 File not found</h2>');
+      }
+    });
+  }
+}
 
-
-    module.exports = handler;
+module.exports = handler;
