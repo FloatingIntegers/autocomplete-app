5509a68301cb857d9c9c2adf7d37c38ef5fb0728
Matt Glover <matthewglover@me.com>
2016-07-01 15:10:29 +0100

    Refactor tests and handler.js to combine frontendTests

--- src/handler.js
+++ src/handler.js
@@ -5,16 +5,7 @@ const getMatchingWords = require('./word-search/get-matching-words');
 function handler(req, res) {
   const url = req.url;
 
-  if (url.includes('frontendTest')) {
-    fs.readFile(`${__dirname}/..${url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end('<h2>404 File not found</h2>');
-      } else {
-        res.end(data);
-      }
-    });
-  } else if (url === '/') {
+  if (url === '/') {
     fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
       if (err) {
         res.writeHead(404);
@@ -24,12 +15,9 @@ function handler(req, res) {
         res.end(data);
       }
     });
-  } else if (url.includes('api/words')) {
-    const queryObj = getQueryParams(url);
-    res.writeHead(200, { 'Content-type': 'text/plain' });
-    getMatchingWords(queryObj.match, queryObj.max).pipe(res);
-  } else if (url.includes('public')) {
-    const ext = url.split('.')[1];
+  } else if (url.includes('public') || url.includes('frontendTest')) {
+    const regExMatch = /\.(\w+)$/.exec(url);
+    const ext = regExMatch !== null ? regExMatch[1] : 'plain';
     fs.readFile(`${__dirname}/..${url}`, (err, data) => {
       if (err) {
         res.writeHead(404);
@@ -39,6 +27,10 @@ function handler(req, res) {
         res.end(data);
       }
     });
+  } else if (url.includes('api/words')) {
+    const queryObj = getQueryParams(url);
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    getMatchingWords(queryObj.match, queryObj.max).pipe(res);
   } else {
     res.writeHead(404);
     res.end('<h2>404 File not found</h2>');
