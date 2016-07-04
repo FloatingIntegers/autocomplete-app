07186cdd7b05e9da6b8aacf9865b522af3cf9d9a
Sofia <emmapohjalainen@gmail.com>
2016-07-04 14:43:55 +0100

    adds 404 messages to display correctly

--- src/handler.js
+++ src/handler.js
@@ -8,7 +8,7 @@ function handler(req, res) {
   if (url === '/') {
     fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
       if (err) {
-        res.writeHead(404);
+        res.writeHead(404, {'Content-type': 'text/html' });
         res.end('<h2>404 File not found</h2>');
       } else {
         res.writeHead(200, { 'Content-type': 'text/html' });
@@ -20,7 +20,7 @@ function handler(req, res) {
     const ext = regExMatch !== null ? regExMatch[1] : 'plain';
     fs.readFile(`${__dirname}/..${url}`, (err, data) => {
       if (err) {
-        res.writeHead(404);
+        res.writeHead(404, {'Content-type': 'text/html' });
         res.end('<h2>404 File not found</h2>');
       } else {
         res.writeHead(200, { 'Content-type': `text/${ext}` });
@@ -32,7 +32,7 @@ function handler(req, res) {
     res.writeHead(200, { 'Content-type': 'text/plain' });
     getMatchingWords(queryObj.match, queryObj.max).pipe(res);
   } else {
-    res.writeHead(404);
+    res.writeHead(404, {'Content-type': 'text/html' });
     res.end('<h2>404 File not found</h2>');
   }
 }
