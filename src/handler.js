ac1f34f07f8fb43fc73ead843cfaa30ab979bd3b
Matt Glover <matthewglover@me.com>
2016-07-04 12:08:44 +0100

    update handler.js to deal with all valid static files and file types

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const getQueryParams = require('./get-query-params');
 const getMatchingWords = require('./word-search/get-matching-words');
 
+const VALID_EXTS_TO_CONTENT_TYPES = {
+  ico: 'image/x-icon',
+};
+
+function getContentTypeFromExt(ext) {
+  return VALID_EXTS_TO_CONTENT_TYPES[ext] || `text/${ext}`;
+}
+
+function getFileExt(url) {
+  const regExMatch = /\.(\w+)$/.exec(url);
+  return regExMatch !== null ? regExMatch[1] : 'plain';
+}
+
 function handler(req, res) {
   const url = req.url;
 
@@ -15,25 +28,21 @@ function handler(req, res) {
         res.end(data);
       }
     });
-  } else if (url.includes('public') || url.includes('frontendTest')) {
-    const regExMatch = /\.(\w+)$/.exec(url);
-    const ext = regExMatch !== null ? regExMatch[1] : 'plain';
-    fs.readFile(`${__dirname}/..${url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.end('<h2>404 File not found</h2>');
-      } else {
-        res.writeHead(200, { 'Content-type': `text/${ext}` });
-        res.end(data);
-      }
-    });
   } else if (url.includes('api/words')) {
     const queryObj = getQueryParams(url);
     res.writeHead(200, { 'Content-type': 'text/plain' });
     getMatchingWords(queryObj.match, queryObj.max).pipe(res);
   } else {
-    res.writeHead(404);
-    res.end('<h2>404 File not found</h2>');
+    const ext = getFileExt(url);
+    fs.readFile(`${__dirname}/..${url}`, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h2>404 File not found</h2>');
+      } else {
+        res.writeHead(200, { 'Content-type': getContentTypeFromExt(ext) });
+        res.end(data);
+      }
+    });
   }
 }
 
